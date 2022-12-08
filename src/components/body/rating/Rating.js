ba2da83e6d062b4dbe4490d845ba63c1add41960
justmarvis <playdeadcanyon@gmail.com>
2022-12-08 13:20:45 -0700

    Edited home page

--- src/components/body/rating/Rating.js
+++ src/components/body/rating/Rating.js
@@ -3,28 +3,28 @@ import './Rating.css';
 function Rating() {
     let position = ["Freelancer", "Front-End Developer", "Full-Stack Developer", "Web Developer", "Back-End Developer"];
 
-    let rating = (5 + 5 + 4 + 3) / 4;
-    let target = rating;
+    // let rating = (5 + 5 + 4 + 3) / 4;
+    // let target = rating;
 
-    const counters = document.querySelectorAll('.ra');
-    const speed = 1000;
+    // const counters = document.querySelectorAll('.ra');
+    // const speed = 1000;
 
-    counters.forEach(counter => {
-        const updateCount = () => {
-            const count = +counter.innerText;
+    // counters.forEach(counter => {
+    //     const updateCount = () => {
+    //         const count = +counter.innerText;
 
-            const inc = target / speed;
+    //         const inc = target / speed;
 
-            if(count < target) {
-                counter.innerText = count + inc;
-                setTimeout(updateCount, 1);
-            } else {}
+    //         if(count < target) {
+    //             counter.innerText = count + inc;
+    //             setTimeout(updateCount, 1);
+    //         } else {}
 
-            // console.log(count);
-        }
+    //         // console.log(count);
+    //     }
 
-        updateCount();
-    });
+    //     updateCount();
+    // });
 
 
     return (
